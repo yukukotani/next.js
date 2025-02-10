6b92e5202b40a2092f11e27b9b813b93af2a4c69
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-02-10 23:02:15 +0100

    Create unique source URLs between Client and Server in Pages dir

--- packages/next/src/build/swc/generated-native.d.ts
+++ packages/next/src/build/swc/generated-native.d.ts
@@ -33,11 +33,6 @@ export class ExternalObject<T> {
     [K: symbol]: T
   }
 }
-export interface TransformOutput {
-  code: string
-  map?: string
-  output?: string
-}
 export declare function mdxCompile(
   value: string,
   option: Buffer,
--- test/development/app-dir/missing-required-html-tags/app/layout.js
+++ test/development/app-dir/missing-required-html-tags/app/layout.js
@@ -1,3 +1,3 @@
 export default function Root({ children }) {
-  return children
+  return <p>hello world</p>
 }
